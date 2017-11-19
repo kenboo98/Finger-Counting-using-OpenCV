cfc803695d0a4f40d758d69fb5cf1cfb508e98c5
kenboo98 <kenboo1998@gmail.com>
2017-11-18 22:40:38 -0700

    First commit.Has base files to input keystrokes and a test

---
+++ browsercontrol.py
@@ -0,0 +1,16 @@
+import pyautogui
+
+
+def pausePlayVideo():
+	pyautogui.press('k')
+def forwardTen():
+	pyautogui.press('l')
+def rewindTen():
+	pyautogui.press('j')
+def mute():
+	pyautogui.press('m')
+def nextVidPlaylist():
+	pyautogui.keyDown('shift')
+	pyautogui.press('n')
+	pyautogui.keyUp('shift')
+	
---
+++ testControls.py
@@ -0,0 +1,32 @@
+import browsercontrol
+import time
+'''time.sleep(5)
+print('pausing')
+browsercontrol.pausePlayVideo()
+
+time.sleep(5)
+print('playing')
+browsercontrol.pausePlayVideo()
+
+time.sleep(5)
+print('forward 10 seconds')
+browsercontrol.forwardTen()
+
+time.sleep(5)
+print('rewind 10 seconds')
+browsercontrol.rewindTen()
+
+time.sleep(5)
+print('muting')
+browsercontrol.mute()
+
+time.sleep(5)
+print('unmuting')
+browsercontrol.mute()
+'''
+time.sleep(5)
+
+browsercontrol.nextVidPlaylist()
+print('next video in playlist')
+
+
